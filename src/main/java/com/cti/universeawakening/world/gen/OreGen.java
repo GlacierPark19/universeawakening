c45e0293535a2c55feacadb6a7a19181d7bdd5bb
GlacierPark19 <55335173+GlacierPark19@users.noreply.github.com>
2020-11-09 07:26:13 -0800

    Update OreGen.java

--- src/main/java/com/cti/universeawakening/world/gen/OreGen.java
+++ src/main/java/com/cti/universeawakening/world/gen/OreGen.java
@@ -16,21 +16,33 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 
-//@Mod.EventBusSubscriber(modid = universeawakening.MODID, bus = Mod.EventBusSubscriber.Bus.MOD )
 
-//public class OreGen {
- //   @SubscribeEvent
-   // public static void generateOres(FMLLoadCompleteEvent event) {
-   // for (Biome biome : ForgeRegistries.BIOMES) {
+public static void setupGen() {
+    for (Map.Entry<RegistryKey<Biome>, Biome> biome : WorldGenRegistries.field_243657_i.func_239659_c_() /* Collection of Biome Entries */) {
+        if (!biome.getValue().getCategory().equals(Biome.Category.NETHER) && !biome.getValue().getCategory().equals(Biome.Category.THEEND)) {
+            addFeatureToBiome(
+                biome.getValue(),
+                GenerationStage.Decoration.UNDERGROUND_ORES,
+                WorldGenRegistries.field_243653_e.getOrDefault(ModBlocks.ADAMANTINE_ORE.getId())
+            );
+        }
+    }
+}
 
-       //world Gen
-      //  genOre(biome, 15, 32, 1, 256, OreFeatureConfig.FillerBlockType.STONE, RegistryHandler.COPPER_ORE.get().getDefaultState(), 8);
+public static void addFeatureToBiome(Biome biome, GenerationStage.Decoration decoration, ConfiguredFeature<?, ?> configuredFeature) {
+    List<List<Supplier<ConfiguredFeature<?, ?>>>> biomeFeatures = new ArrayList<>(
+        biome.func_242440_e().func_242498_c() /* List of Configured Features */
+    );
 
-   // }
+    while (biomeFeatures.size() <= decoration.ordinal()) {
+        biomeFeatures.add(Lists.newArrayList());
+    }
+    
+    List<Supplier<ConfiguredFeature<?, ?>>> features = new ArrayList<>(biomeFeatures.get(decoration.ordinal()));
+    features.add(() -> configuredFeature);
+    biomeFeatures.set(decoration.ordinal(), features);
 
-
-//};
-
-//}
-
+    /* Change field_242484_f that contains the Configured Features of the Biome*/
+    ObfuscationReflectionHelper.setPrivateValue(BiomeGenerationSettings.class, biome.func_242440_e(), biomeFeatures, "field_242484_f");
+}}
 
